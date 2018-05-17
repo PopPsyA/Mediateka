1924d3ee501578c7cc0112263ad76795f3489d09
Devit951 <Devit951@gmail.com>
2018-05-17 20:45:59 +0500

    Changed all background screen resoliton(from 500p to 1280p)

--- app/src/main/java/com/ru/devit/mediateka/utils/Constants.java
+++ app/src/main/java/com/ru/devit/mediateka/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.ru.devit.mediateka.utils;
 
 public final class Constants {
-    public static final String IMG_PATH_W500 = "https://image.tmdb.org/t/p/w500/";
+    public static final String IMG_PATH_W1280 = "https://image.tmdb.org/t/p/w1280/";
     public static final String IMG_PATH_W185 = "https://image.tmdb.org/t/p/w185/";
     public static final String DEFAULT_VALUE = "N/A";
 }
