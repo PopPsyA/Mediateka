0acabb9cc148f4f25487247c414ce430c04c627c
Devit951 <Devit951@gmail.com>
2018-05-24 12:20:34 +0500

    Updated Room version

--- app/src/main/java/com/ru/devit/mediateka/presentation/posterslider/PosterSliderAdapter.java
+++ app/src/main/java/com/ru/devit/mediateka/presentation/posterslider/PosterSliderAdapter.java
@@ -4,7 +4,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
