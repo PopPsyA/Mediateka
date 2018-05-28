24904fdefeb85b64cfaa5d02fea85b692141178f
Devit951 <Devit951@gmail.com>
2018-05-28 20:08:47 +0500

    Added slide animation

--- app/src/main/java/com/ru/devit/mediateka/presentation/widget/IndicatorView.java
+++ app/src/main/java/com/ru/devit/mediateka/presentation/widget/IndicatorView.java
@@ -8,7 +8,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import com.ru.devit.mediateka.R;
@@ -21,9 +20,10 @@ public class IndicatorView extends View implements ViewPager.OnPageChangeListene
     private Paint mSelectedIndicatorPaint;
     private Paint mDefaultIndicatorPaint;
     private int indicatorCount;
-    private int diametr;
+    private int diameter;
     private int size;
     private int currentPosition;
+    private float offset;
 
     public IndicatorView(Context context) {
         super(context);
@@ -43,12 +43,15 @@ public class IndicatorView extends View implements ViewPager.OnPageChangeListene
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int radius = diametr / 2;
+        int radius = diameter / 2;
         for (int i = 0; i < indicatorCount; i++){
-            int cx = diametr + (size * i);
-            canvas.drawCircle(cx , diametr, radius , mDefaultIndicatorPaint);
+            int cx = diameter + (size * i);
+            canvas.drawCircle(cx , diameter, radius , mDefaultIndicatorPaint);
         }
-        canvas.drawCircle(diametr + (size * currentPosition) , diametr, radius , mSelectedIndicatorPaint);
+        float selectedPosition = (size * currentPosition);
+        float nextPosition = size * (currentPosition + 1);
+
+        canvas.drawCircle(diameter + lerp(selectedPosition , nextPosition , offset) , diameter, radius , mSelectedIndicatorPaint);
     }
 
     @Override
@@ -58,13 +61,14 @@ public class IndicatorView extends View implements ViewPager.OnPageChangeListene
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+        offset = positionOffset;
+        currentPosition = position;
+        invalidate();
     }
 
     @Override
     public void onPageSelected(int position) {
-        currentPosition = position;
-        invalidate();
+
     }
 
     @Override
@@ -77,6 +81,10 @@ public class IndicatorView extends View implements ViewPager.OnPageChangeListene
         mViewPager.addOnPageChangeListener(this);
         indicatorCount = Objects.requireNonNull(mViewPager.getAdapter()).getCount();
         requestLayout();
+    }
+
+    private float lerp(float firstPosition, float secondPosition , float offset){
+        return firstPosition + offset * (secondPosition - firstPosition);
     }
 
     private int measureWidth(int measureSpec){
@@ -103,7 +111,7 @@ public class IndicatorView extends View implements ViewPager.OnPageChangeListene
         if (heightMode == MeasureSpec.EXACTLY){
             result = heightSize;
         } else {
-            result = 2 * diametr + getPaddingTop() + getPaddingBottom();
+            result = 2 * diameter + getPaddingTop() + getPaddingBottom();
             if (heightMode == MeasureSpec.AT_MOST){
                 result = Math.min(result , heightSize);
             }
@@ -115,13 +123,13 @@ public class IndicatorView extends View implements ViewPager.OnPageChangeListene
         TypedArray a = context.obtainStyledAttributes(attrs , R.styleable.IndicatorView);
         int defaultIndicatorColor = a.getColor(R.styleable.IndicatorView_iv_default_color , ContextCompat.getColor(context , R.color.colorWhite));
         int selectedIndicatorColor = a.getColor(R.styleable.IndicatorView_iv_selected_color , ContextCompat.getColor(context , R.color.colorWhite));
-        diametr = a.getDimensionPixelSize(R.styleable.IndicatorView_iv_diametr , getResources().getDimensionPixelSize(R.dimen.indicator_diametr));
-        size = diametr * 2;
+        diameter = a.getDimensionPixelSize(R.styleable.IndicatorView_iv_diametr , getResources().getDimensionPixelSize(R.dimen.indicator_diametr));
+        size = diameter * 2;
 
         mDefaultIndicatorPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mDefaultIndicatorPaint.setColor(defaultIndicatorColor);
         mDefaultIndicatorPaint.setStyle(Paint.Style.STROKE);
-        mDefaultIndicatorPaint.setStrokeWidth(1.5f);
+        mDefaultIndicatorPaint.setStrokeWidth(2f);
         mSelectedIndicatorPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mSelectedIndicatorPaint.setColor(selectedIndicatorColor);
         a.recycle();
