fbee116c66021b5e30ef4e86d4861c7095fa230d
Devit951 <Devit951@gmail.com>
2018-05-30 17:47:07 +0500

    Fixed bug when text not ellipsized

--- app/src/main/java/com/ru/devit/mediateka/presentation/widget/CutableTextView.java
+++ app/src/main/java/com/ru/devit/mediateka/presentation/widget/CutableTextView.java
@@ -1,18 +1,13 @@
 package com.ru.devit.mediateka.presentation.widget;
 
 import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
 import android.content.Context;
-import android.graphics.Canvas;
-import android.support.v4.view.animation.FastOutLinearInInterpolator;
 import android.support.v7.widget.AppCompatTextView;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
-import android.view.ViewGroup;
-import android.view.animation.AnticipateOvershootInterpolator;
 
 import com.ru.devit.mediateka.R;
 
@@ -22,7 +17,7 @@ public class CutableTextView extends AppCompatTextView {
     private static final String PROPERTY_MAX_LINES = "maxLines";
 
     private boolean clicked = false;
-    private boolean isLinesSetted = false;
+    private boolean linesRendered = false;
     private int totalCountLines;
 
     public CutableTextView(Context context) {
@@ -40,20 +35,18 @@ public class CutableTextView extends AppCompatTextView {
         init(context);
     }
 
-
     @Override
     public void setText(CharSequence text, BufferType type) {
         super.setText(text, type);
-    }
-
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-        if (getMaxLines() != SHORT_TEXT_LINES && !isLinesSetted){
-            totalCountLines = getLineCount();
-            isLinesSetted = true;
-        }
-        setMaxLines(SHORT_TEXT_LINES);
+        post(() -> {
+            if (getLineCount() > SHORT_TEXT_LINES && !linesRendered){
+                totalCountLines = getLineCount();
+                linesRendered = true;
+            }
+            if (totalCountLines > SHORT_TEXT_LINES){
+                setLines(SHORT_TEXT_LINES);
+            }
+        });
     }
 
     @Override
@@ -79,14 +72,16 @@ public class CutableTextView extends AppCompatTextView {
     }
 
     private void startMaxLineAnimationChanger(int from , int to) {
-        ObjectAnimator objectAnimator = ObjectAnimator.ofInt(this, PROPERTY_MAX_LINES, from , to);
-        if (to == 0){
-            objectAnimator = ObjectAnimator.ofInt(this , PROPERTY_MAX_LINES , from);
+        if (totalCountLines > SHORT_TEXT_LINES){
+            ObjectAnimator objectAnimator = ObjectAnimator.ofInt(this, PROPERTY_MAX_LINES, from , to);
+            if (to == 0){
+                objectAnimator = ObjectAnimator.ofInt(this , PROPERTY_MAX_LINES , from);
+            }
+            if (totalCountLines <= 8){
+                objectAnimator.setDuration(70);
+            }
+            objectAnimator.start();
         }
-        if (totalCountLines <= 8){
-            objectAnimator.setDuration(70);
-        }
-        objectAnimator.start();
     }
 
     private void init(Context context) {
