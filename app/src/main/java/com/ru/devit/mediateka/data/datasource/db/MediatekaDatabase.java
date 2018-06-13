f1db3f4024aa41c14a5a30f437482175636a2056
Devit951 <Devit951@gmail.com>
2018-06-13 17:42:00 +0500

    Renamed AppDatabase to MediatekaDatabase

--- app/src/main/java/com/ru/devit/mediateka/data/datasource/db/MediatekaDatabase.java
+++ app/src/main/java/com/ru/devit/mediateka/data/datasource/db/MediatekaDatabase.java
@@ -2,7 +2,9 @@ package com.ru.devit.mediateka.data.datasource.db;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 
+import com.ru.devit.mediateka.models.IntArrayConverter;
 import com.ru.devit.mediateka.models.db.ActorEntity;
 import com.ru.devit.mediateka.models.db.CinemaActorJoinEntity;
 import com.ru.devit.mediateka.models.db.CinemaEntity;
@@ -10,7 +12,8 @@ import com.ru.devit.mediateka.models.db.CinemaEntity;
 @Database(entities = {CinemaEntity.class , ActorEntity.class , CinemaActorJoinEntity.class} ,
         version = 1 ,
         exportSchema = false)
-public abstract class AppDatabase extends RoomDatabase {
+@TypeConverters(IntArrayConverter.class)
+public abstract class MediatekaDatabase extends RoomDatabase {
     public abstract CinemaDao getCinemaDao();
     public abstract ActorDao getActorDao();
     public abstract CinemaActorJoinDao getCinemaActorJoinDao();
