411c517d0ff9eb23351cdd9d5f02f351f1886722
Devit951 <Devit951@gmail.com>
2018-06-18 12:51:43 +0500

    Added GetFavouriteListCinema use case

---
+++ app/src/main/java/com/ru/devit/mediateka/domain/cinemausecases/GetFavouriteListCinema.java
@@ -0,0 +1,41 @@
+package com.ru.devit.mediateka.domain.cinemausecases;
+
+import com.ru.devit.mediateka.data.repository.cinema.CinemaLocalRepository;
+import com.ru.devit.mediateka.domain.UseCase;
+import com.ru.devit.mediateka.models.model.Cinema;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class GetFavouriteListCinema extends UseCase<List<Cinema>> {
+
+    private final CinemaLocalRepository repository;
+
+    @Inject
+    public GetFavouriteListCinema(@Named("executor_thread") Scheduler executorThread ,
+                                  @Named("ui_thread") Scheduler uiThread ,
+                                  CinemaLocalRepository repository) {
+        super(executorThread, uiThread);
+        this.repository = repository;
+    }
+
+    @Override
+    protected Flowable<List<Cinema>> createUseCase() {
+        return repository.getFavouriteListCinema()
+                .toFlowable();
+    }
+
+    public Completable saveFavouriteCinema(final int cinemaId){
+        return repository.saveIntoDatabaseFavouriteCinema(cinemaId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+}
