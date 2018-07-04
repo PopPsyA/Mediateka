c5d66b2ded6b5ef409d0f0cf9f29ea1d21093871
Devit951 <Devit951@gmail.com>
2018-07-04 13:05:53 +0500

    Search activity can dynamically change color of their tab's

--- app/src/main/java/com/ru/devit/mediateka/presentation/search/SearchActivity.java
+++ app/src/main/java/com/ru/devit/mediateka/presentation/search/SearchActivity.java
@@ -1,7 +1,11 @@
 package com.ru.devit.mediateka.presentation.search;
 
+import android.content.res.ColorStateList;
+import android.support.annotation.ColorRes;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -15,6 +19,7 @@ import com.ru.devit.mediateka.presentation.common.ViewPagerAdapter;
 import com.ru.devit.mediateka.presentation.base.BaseActivity;
 import com.ru.devit.mediateka.presentation.actorlist.ActorsFragment;
 import com.ru.devit.mediateka.presentation.smallcinemalist.SmallCinemasFragment;
+import com.ru.devit.mediateka.utils.AnimUtils;
 
 import javax.inject.Inject;
 
@@ -23,6 +28,7 @@ public class SearchActivity extends BaseActivity implements SearchPresenter.View
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
     private EditText mSearchField;
+    private AppBarLayout mAppBar;
 
     private SmallCinemasFragment mCinemasFragment;
     private ActorsFragment mActorsFragment;
@@ -50,16 +56,20 @@ public class SearchActivity extends BaseActivity implements SearchPresenter.View
     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
         String query = charSequence.toString();
         presenter.onTextChanged(query);
+
+        //TODO make icon exit
     }
 
     @Override
     public void onCinemaTabSelected() {
         mSearchField.setHint(getString(R.string.search_cinemas));
+        dynamicallyChangeColor(R.color.colorPurple , R.color.colorDarkPurple);
     }
 
     @Override
     public void onActorTabSelected() {
         mSearchField.setHint(getString(R.string.search_actors));
+        dynamicallyChangeColor(R.color.colorLightBlue , R.color.colorDarkBlue);
     }
 
     @Override
@@ -87,7 +97,9 @@ public class SearchActivity extends BaseActivity implements SearchPresenter.View
         mViewPager = findViewById(R.id.view_pager);
         mTabLayout = findViewById(R.id.tab_layout);
         mSearchField = findViewById(R.id.et_search_field);
+        mAppBar = findViewById(R.id.search_app_bar);
         setUpViewPager(mViewPager);
+        changeStatusBarColor(R.color.colorDarkPurple);
     }
 
     @Override
@@ -139,6 +151,14 @@ public class SearchActivity extends BaseActivity implements SearchPresenter.View
 
     }
 
+    private void dynamicallyChangeColor(@ColorRes int color , @ColorRes int statusBarColor) {
+        int parsedColor = ContextCompat.getColor(this , color);
+        AnimUtils.startRevealAnimationWithOutVisibility(mAppBar);
+        changeStatusBarColor(statusBarColor);
+        mTabLayout.setBackgroundColor(parsedColor);
+        getToolbar().setBackgroundColor(parsedColor);
+    }
+
     private void setUpViewPager(ViewPager viewPager){
         final ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         mCinemasFragment = SmallCinemasFragment.newInstance(true);
@@ -148,6 +168,4 @@ public class SearchActivity extends BaseActivity implements SearchPresenter.View
         viewPager.setAdapter(adapter);
         mTabLayout.setupWithViewPager(mViewPager);
     }
-
-
 }
