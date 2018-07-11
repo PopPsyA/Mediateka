9c0984a322396c63935fad8cf77a543ce8b7b03b
Devit951 <Devit951@gmail.com>
2018-07-11 15:36:23 +0500

    Realized GetActors use case

--- app/src/main/java/com/ru/devit/mediateka/domain/actorusecases/GetActors.java
+++ app/src/main/java/com/ru/devit/mediateka/domain/actorusecases/GetActors.java
@@ -1,23 +1,28 @@
 package com.ru.devit.mediateka.domain.actorusecases;
 
+import com.ru.devit.mediateka.domain.ActorRepository;
 import com.ru.devit.mediateka.domain.UseCase;
 import com.ru.devit.mediateka.models.model.Actor;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import io.reactivex.Flowable;
 import io.reactivex.Scheduler;
-import io.reactivex.Single;
 
 public class GetActors extends UseCase<List<Actor>> {
 
+    private final ActorRepository repository;
 
-    public GetActors(Scheduler executorThread, Scheduler uiThread) {
+    @Inject public GetActors(Scheduler executorThread, Scheduler uiThread, ActorRepository repository) {
         super(executorThread, uiThread);
+        this.repository = repository;
     }
 
     @Override
     protected Flowable<List<Actor>> createUseCase() {
-        return null;
+        return repository.getPopularActors(1)
+                .toFlowable();
     }
 }
