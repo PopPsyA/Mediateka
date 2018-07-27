e38415ffeb73fb88ea8d1ab99cab130c030f9bb0
Devit951 <Devit951@gmail.com>
2018-07-27 16:48:10 +0500

    Added mapOf comparators for sorting cinema list

--- app/src/main/java/com/ru/devit/mediateka/domain/cinemausecases/GetFavouriteListCinema.java
+++ app/src/main/java/com/ru/devit/mediateka/domain/cinemausecases/GetFavouriteListCinema.java
@@ -4,7 +4,10 @@ import com.ru.devit.mediateka.data.repository.cinema.CinemaLocalRepository;
 import com.ru.devit.mediateka.domain.UseCase;
 import com.ru.devit.mediateka.models.model.Cinema;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -16,6 +19,18 @@ import io.reactivex.Scheduler;
 public class GetFavouriteListCinema extends UseCase<List<Cinema>> {
 
     private final CinemaLocalRepository repository;
+    private final Map<Integer , Comparator<Cinema>> mapOfCinemaComparators = new HashMap<Integer, Comparator<Cinema>>(){
+        {
+            put(SORT_BY_CINEMA_DATE, (o1, o2) -> o2.getReleaseDate().compareTo(o1.getReleaseDate()));
+            put(SORT_BY_CINEMA_TITLE , (o1, o2) -> o1.getTitle().compareTo(o2.getTitle()));
+            put(SORT_BY_CINEMA_GENRE , (o1, o2) -> o2.getGenres().length - o1.getGenres().length);
+        }
+    };
+
+    private static final int SORT_BY_CINEMA_DATE = 0;
+    private static final int SORT_BY_CINEMA_TITLE = 1;
+    private static final int SORT_BY_CINEMA_GENRE = 2;
+
 
     @Inject
     public GetFavouriteListCinema(@Named("executor_thread") Scheduler executorThread ,
@@ -48,5 +63,9 @@ public class GetFavouriteListCinema extends UseCase<List<Cinema>> {
     public Completable clearFavouriteList() {
         return repository.clearFavouriteListCinema()
                 .compose(applyCompletableSchedulers());
+    }
+
+    public Comparator<Cinema> createCinemaListComparator(int position) {
+        return mapOfCinemaComparators.get(position);
     }
 }
