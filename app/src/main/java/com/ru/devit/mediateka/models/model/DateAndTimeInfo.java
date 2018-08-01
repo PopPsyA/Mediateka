faa04d12eaea5e17b466bc1f5677e521de9a8d22
Devit951 <Devit951@gmail.com>
2018-08-01 17:27:21 +0500

    Added pojo class DateAndTimeInfo

---
+++ app/src/main/java/com/ru/devit/mediateka/models/model/DateAndTimeInfo.java
@@ -0,0 +1,50 @@
+package com.ru.devit.mediateka.models.model;
+
+public class DateAndTimeInfo {
+
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+}
