a181263345ce415b6b8cf3aa2fcd47c4b4cb841a
Devit951 <Devit951@gmail.com>
2018-08-06 13:41:25 +0500

    Encapsulated logic and delegeted to Quality enum

--- app/src/main/java/com/ru/devit/mediateka/utils/UrlImagePathCreator.java
+++ app/src/main/java/com/ru/devit/mediateka/utils/UrlImagePathCreator.java
@@ -6,23 +6,20 @@ public final class UrlImagePathCreator {
     private static final String IMG_PATH_W780 = "https://image.tmdb.org/t/p/w780/";
     private static final String IMG_PATH_W185 = "https://image.tmdb.org/t/p/w185/";
 
-    public static String create1280pPictureUrl(String imgUrl){
-        return IMG_PATH_W1280 + imgUrl;
-    }
-
-    public static String create780pPictureUrl(String imgUrl){
-        return IMG_PATH_W780 + imgUrl;
-    }
-
-    public static String create185pPictureUrl(String imgUrl){
-        return IMG_PATH_W185 + imgUrl;
+    public static String createPictureUrlFromQuality(Quality qualityType , String imgUrl){
+        for (Quality quality : Quality.values()){
+            if (quality.equals(qualityType)){
+                return qualityType.quality(imgUrl);
+            }
+        }
+        throw new IllegalArgumentException("No such " + qualityType);
     }
 
     public static String createPictureUrlFromQuality(String imgUrl , String quality){
-        return Quality.valueOf("Quality" + quality).quality(imgUrl);
+        return Quality.valueOf(Quality.class.getSimpleName() + quality).quality(imgUrl);
     }
 
-    enum Quality{
+    public enum Quality{
         Quality1280 {
             @Override
             public String quality(String imgUrl) {
@@ -43,5 +40,17 @@ public final class UrlImagePathCreator {
         };
 
         public abstract String quality(String imgUrl);
+    }
+
+    private static String create1280pPictureUrl(String imgUrl){
+        return IMG_PATH_W1280 + imgUrl;
+    }
+
+    private static String create780pPictureUrl(String imgUrl){
+        return IMG_PATH_W780 + imgUrl;
+    }
+
+    private static String create185pPictureUrl(String imgUrl){
+        return IMG_PATH_W185 + imgUrl;
     }
 }
